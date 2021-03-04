f2cad2f7662bffcd111a8f859a7f7677eeac9bc4
Cha'sMacAir <chayh311@gmail.com>
2021-03-04 13:22:38 +0900

    feat: Initiate first cra settings & Add a categories component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
 function App() {
+  const [openFirst, setOpenFirst] = useState(false);
+  const [openSecond, setOpenSecond] = useState(false);
+
+  const [secondCts, setSecondCts] = useState<any[]>([]);
+  const over = () => {
+    setOpenFirst(true)
+  }
+  const leave = () => {
+    setOpenFirst(false);
+    setOpenSecond(false);
+  }
+
+  const [cts, setCts] = useState<any[]>([]);
+  const getCateg = async () => {
+    const _cts = await axios.get('https://api.kurly.com/v2/categories?ver=1');
+
+    setCts(_cts.data.data.categories);
+  }
+  useEffect(() => {
+    getCateg();
+  }, [])
+
+  const firstSectionOver = (no: string) => {
+    getSecondCts(no)
+    setOpenSecond(true);
+  }
+
+  const getSecondCts = (no: string) => {
+    const index = cts.findIndex(item => item.no === no);
+    setSecondCts(cts[index].categories);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <AppBlock className="App">
+      <div onMouseOver={over} onMouseLeave={leave} >
+        <button>전체 카테고리</button>
+        <div className="flex">
+          {openFirst && <div>{cts.map(item => <p onMouseOver={() => firstSectionOver(item.no)}>{item.name}</p>)}</div>}
+          {openSecond  && <div>{secondCts.map(item => <p>{item.name}</p>)}</div>}
+        </div>
+
+      </div>
+    </AppBlock>
   );
 }
 
+const AppBlock = styled.div`
+  display: inline-block;
+  >button {
+    width: 8rem;
+  }
+
+  .flex {
+    display: flex;
+
+  }
+`;
 export default App;
